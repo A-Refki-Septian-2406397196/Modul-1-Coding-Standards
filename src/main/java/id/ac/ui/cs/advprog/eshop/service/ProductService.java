fd6ab744530ac8e2e10a796820958efbd03275ef
RefkiSeptian <refkiseptianrs@gmail.com>
2026-02-10 19:04:22 +0700

    fixing minor bug

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface ProductService {
     Product create(Product product);
