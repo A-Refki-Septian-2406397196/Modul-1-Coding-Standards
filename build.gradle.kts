7967cdf7801f77a2c8b78a306ce404ede6a3d45c
RefkiSeptian <refkiseptianrs@gmail.com>
2026-02-21 12:05:44 +0700

    minor fix at sonarcloud script/2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     jacoco
+    id("org.sonarqube") version "5.0.0.4638"
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 }
