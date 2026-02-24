fdf2b83061e2ea04d9c5a642dcb252d5a6cb2555
RefkiSeptian <refkiseptianrs@gmail.com>
2026-02-24 18:02:09 +0700

    finished tutorial 2

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -38,23 +38,16 @@ class HomePageFunctionalTest {
     }
 
     @Test
-    void pageTitle_isCorrect(ChromeDriver driver) throws Exception {
-        // Exercise
+    void pageTitle_isCorrect(ChromeDriver driver) {
         driver.get(baseUrl);
         String pageTitle = driver.getTitle();
-
-        // Verify
         assertEquals("ADV Shop", pageTitle);
     }
 
     @Test
-    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
-        // Exercise
+    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) {
         driver.get(baseUrl);
-        String welcomeMessage = driver.findElement(By.tagName("h3"))
-                .getText();
-
-        // Verify
+        String welcomeMessage = driver.findElement(By.tagName("h3")).getText();
         assertEquals("Welcome", welcomeMessage);
     }
 }
